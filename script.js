9ddb411e3905350b965b83f8dd7c70f28ce1926b
dnaixl <108243327+dnaixl@users.noreply.github.com>
2024-02-18 17:53:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ function calculateTotal() {
     let totalAge = ages.reduce((acc, curr) => acc + curr, 0);
     consoleOutput.textContent += `Total age is ${totalAge}`;
 }
+
+
+function changeBackground() {
+    let images = [
+        'https://images.unsplash.com/photo-1699755094450-620e32ec86a6?q=80&w=1636&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1705517649861-5450dc4dd44a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1706463661223-4e7007549823?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1707497009301-78bed8b1a51d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    ];
+
+    let randomIndex = Math.floor(Math.random() * images.length);
+    document.body.style.backgroundImage = `url(${images[randomIndex]})`;
+}
+
